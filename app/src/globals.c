916dd9cfe745267594cfcca0df6ce88a60ad3d2d
Benjamin Canou <benjamin@canou.fr>
2023-04-11 13:30:43 +0200

    Add copyright lines to files inherited from previous app

--- app/src/globals.c
+++ app/src/globals.c
@@ -1,5 +1,9 @@
 /* Copyright 2023 Nomadic Labs <contact@nomadic-labs.com>
 
+   With code excerpts from:
+    - Legacy Tezos app, Copyright 2019 Obsidian Systems
+    - Ledger Blue sample apps, Copyright 2016 Ledger
+
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
    You may obtain a copy of the License at
