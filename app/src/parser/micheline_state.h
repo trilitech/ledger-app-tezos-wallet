752d83ccae45ba2276c237e44c2cf78b85d13d22
Benjamin Canou <benjamin@canou.fr>
2023-04-12 14:56:23 +0200

    Add minimal description in C headers or parser

--- app/src/parser/micheline_state.h
+++ app/src/parser/micheline_state.h
@@ -1,4 +1,6 @@
-/* Copyright 2023 Nomadic Labs <contact@nomadic-labs.com>
+/* Tezos Embedded C parser for Ledger - Parser state for Micheline data
+
+   Copyright 2023 Nomadic Labs <contact@nomadic-labs.com>
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
