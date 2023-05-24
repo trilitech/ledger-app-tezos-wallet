7eff3114c118b09f1ae1936c4867416d7da3b49c
Sébastien Palmer <sebastien.palmer@functori.com>
2023-05-24 17:17:43 +0200

    Tests: move the value for the apdu into its own module

--- tests/generate/gen_integration.ml
+++ tests/generate/gen_integration.ml
@@ -40,107 +40,13 @@ let sk =
   Tezos_crypto.Signature.Secret_key.of_b58check_exn
     "edsk2tUyhVvGj9B1S956ZzmaU4bC9J7t8xVBH52fkAoZL25MHEwacd"
 
-module Class = struct
-  type t = Default
-
-  let to_uint8 = function Default -> 0x80
-end
-
-module Instruction = struct
-  type t =
-    | Version
-    | Authorize_baking
-    | Get_public_key
-    | Prompt_public_key
-    | Sign
-    | Sign_unsafe
-    | Reset
-    | Query_auth_key
-    | Query_main_hwm
-    | Git
-    | Setup
-    | Query_all_hwm
-    | Deauthorize
-    | Query_auth_key_with_curve
-    | Hmac
-    | Sign_with_hash
-
-  let to_uint8 = function
-    | Version -> 0x00
-    | Authorize_baking -> 0x01
-    | Get_public_key -> 0x02
-    | Prompt_public_key -> 0x03
-    | Sign -> 0x04
-    | Sign_unsafe -> 0x05
-    | Reset -> 0x06
-    | Query_auth_key -> 0x07
-    | Query_main_hwm -> 0x08
-    | Git -> 0x09
-    | Setup -> 0x0A
-    | Query_all_hwm -> 0x0B
-    | Deauthorize -> 0x0C
-    | Query_auth_key_with_curve -> 0x0D
-    | Hmac -> 0x0E
-    | Sign_with_hash -> 0x0F
-end
-
-module Index = struct
-  type t = int
-
-  let to_uint8 ?(is_last = false) idx =
-    let is_last = if is_last then 0x80 else 0x00 in
-    is_last lor idx
-end
-
-module Curve = struct
-  type t = ED25519 | SECP256K1 | SECP256R1 | BIP32_ED25519
-
-  let to_uint8 = function
-    | ED25519 -> 0x00
-    | SECP256K1 -> 0x01
-    | SECP256R1 -> 0x02
-    | BIP32_ED25519 -> 0x03
-end
-
-let apdu_success = Bytes.of_string "\x90\x00"
-let max_apdu_size = 235
-
-let make_packet ?is_last ?(idx = 0) ~cla ~ins ~curve content =
-  let content_length = Bytes.length content in
-  assert (content_length <= max_apdu_size);
-  let header = Bytes.create 5 in
-  Bytes.set_uint8 header 0 @@ Class.to_uint8 cla;
-  Bytes.set_uint8 header 1 @@ Instruction.to_uint8 ins;
-  Bytes.set_uint8 header 2 @@ Index.to_uint8 ?is_last idx;
-  Bytes.set_uint8 header 3 @@ Curve.to_uint8 curve;
-  Bytes.set_uint8 header 4 content_length;
-  Bytes.cat header content
-
-let make_packets ?(idx = 0) ~cla ~ins ~curve content =
-  let len = Bytes.length content in
-  let rec aux ofs idx =
-    let remaining_size = len - ofs in
-    if remaining_size <= max_apdu_size then
-      let packet =
-        make_packet ~is_last:true ~idx ~cla ~ins ~curve
-        @@ Bytes.sub content ofs remaining_size
-      in
-      [ packet ]
-    else
-      let packet =
-        make_packet ~idx ~cla ~ins ~curve @@ Bytes.sub content ofs max_apdu_size
-      in
-      packet :: aux (ofs + max_apdu_size) (idx + 1)
-  in
-  aux 0 idx
-
 let split_sign_apdus bin =
-  let cla = Class.Default in
-  let ins = Instruction.Sign_with_hash in
-  let curve = Curve.ED25519 in
+  let cla = Apdu.Class.Default in
+  let ins = Apdu.Instruction.Sign_with_hash in
+  let curve = Apdu.Curve.ED25519 in
   let apdus =
-    make_packet ~cla ~ins ~curve path
-    :: make_packets ~idx:1 ~cla ~ins ~curve bin
+    Apdu.make_packet ~cla ~ins ~curve path
+    :: Apdu.make_packets ~idx:1 ~cla ~ins ~curve bin
   in
   let expectations =
     let bin_sign_success_bytes =
@@ -148,9 +54,9 @@ let split_sign_apdus bin =
       let sign =
         Tezos_crypto.Signature.to_bytes (Tezos_crypto.Signature.sign sk bin)
       in
-      Bytes.concat Bytes.empty [ bin_hash; sign; apdu_success ]
+      Bytes.concat Bytes.empty [ bin_hash; sign; Apdu.success ]
     in
-    List.init (List.length apdus - 1) (fun _ -> apdu_success)
+    List.init (List.length apdus - 1) (fun _ -> Apdu.success)
     @ [ bin_sign_success_bytes ]
   in
   List.combine apdus expectations
