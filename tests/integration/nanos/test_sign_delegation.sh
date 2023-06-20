de70bfed4970d77ca00df5f339ed2b6d8aafe44c
Sébastien Palmer <sebastien.palmer@functori.com>
2023-06-20 13:05:09 +0200

    Tests: add basic sign delegation tests

---
+++ tests/integration/nanos/test_sign_delegation.sh
@@ -0,0 +1,19 @@
+# full input: 0300000000000000000000000000000000000000000000000000000000000000006e01774d99da021b92d8c3dfc2e814c7658440319be2c09a0cf40509f906ff00591e842444265757d6a65e3670ca18b5e662f9c0
+# signer: tz1dyX3B1CFYa2DfdFLyPtiJCfQRUgPVME6E
+expect_full_text 'Tezos Wallet' 'ready for' 'safe signing'
+send_async_apdus \
+	800f000011048000002c800006c18000000080000000 "expect_apdu_return 9000
+"\
+	800f8100550300000000000000000000000000000000000000000000000000000000000000006e01774d99da021b92d8c3dfc2e814c7658440319be2c09a0cf40509f906ff00591e842444265757d6a65e3670ca18b5e662f9c0 "expect_apdu_return a6b0ce461c2c49d22cc9ec552e2cec1f5648c724012d53384f07a5134366dcc0be83b0502234bb618de1b8e5585c3344332284858cc799f3e2f58820ba2e6f66c2403d3084b0ab6ae6400733a912448e7614fbab139b532f081da7bcf12f5b029000
+"
+expect_full_text 'Operation (0)' 'Delegation'
+press_button right
+expect_full_text 'Fee' '0.2 tz'
+press_button right
+expect_full_text 'Storage limit' '889'
+press_button right
+expect_full_text 'Delegate' 'tz1TmFPVZsGQ8MnrBJtnECJgkFUwLa6EWYDm'
+press_button right
+expect_full_text 'Accept?'
+press_button both
+expect_async_apdus_sent
