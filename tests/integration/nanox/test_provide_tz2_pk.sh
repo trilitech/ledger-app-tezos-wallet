63c3bf91092743af6d764c513d18f69a1593512e
Roland C. Dowdeswell <elric@imrryr.org>
2023-08-22 18:47:43 +0100

    Partially revert "Tests: suspend tz2 signing tests"
    This reverts commit d30c6cc4e3129e718fcb45049f5fe7532e745b0f.
    
    We put some of the tz2 tests back in.  We do not, however, add them
    to the ML generation.

---
+++ tests/integration/nanox/test_provide_tz2_pk.sh
@@ -0,0 +1,12 @@
+start_speculos "$seed"
+sleep 0.2
+expect_full_text "Tezos Wallet" "ready for" "safe signing"
+send_apdu 8003000111048000002c800006c18000000080000000
+expect_full_text "Provide Key" 'tz2GB5YHqF4UzQ8GP5yUqdhY9oVWRXCY2hPU'
+press_button right
+expect_full_text "Accept?" "Press both buttons to accept."
+press_button both
+expect_apdu_return 4104211f369d9ec3a0fbe10febf05a8b67a9ca705b90534b9dfe3e08f2dac99bd008111079d35265a935866e0444f1a4c1044c9c81f4178d43ef5c132332019bf9c19000
+press_button right
+press_button both
+expect_exited
