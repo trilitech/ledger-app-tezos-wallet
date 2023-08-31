2287cd21625b550f0a3198791e4451d20490b15e
Roland C. Dowdeswell <elric@imrryr.org>
2023-08-31 16:01:57 +0100

    nanos/test_provide_tz2_pk.sh was missing a right button press

--- tests/integration/nanos/test_provide_tz2_pk.sh
+++ tests/integration/nanos/test_provide_tz2_pk.sh
@@ -10,5 +10,6 @@ expect_full_text "Accept?"
 press_button both
 expect_apdu_return 4104211f369d9ec3a0fbe10febf05a8b67a9ca705b90534b9dfe3e08f2dac99bd008111079d35265a935866e0444f1a4c1044c9c81f4178d43ef5c132332019bf9c19000
 press_button right
+press_button right
 press_button both
 expect_exited
