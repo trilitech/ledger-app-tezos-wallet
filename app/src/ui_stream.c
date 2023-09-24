c374d00bbe55c0670de04a23fed38c302d1519a5
Emma Turner <emma.turner@trili.tech>
2023-09-24 20:51:38 +0100

    stax: disable nano accept/reject screens
    on stax, the 'reject' option is always visible/available during the
    course of a review, and the accept screen is also handled by a
    specific 'builtin' signing page.
    
    therefore, we disable both screens from being pushed when on stax

--- app/src/ui_stream.c
+++ app/src/ui_stream.c
@@ -51,10 +51,12 @@ void
 tz_ui_stream_push_accept_reject(void)
 {
     FUNC_ENTER(("void"));
+#ifdef HAVE_BAGL
     tz_ui_stream_push(TZ_UI_STREAM_CB_ACCEPT, "Accept?",
                       "Press both buttons to accept.", TZ_UI_ICON_TICK);
     tz_ui_stream_push(TZ_UI_STREAM_CB_REJECT, "Reject?",
                       "Press both buttons to reject.", TZ_UI_ICON_CROSS);
+#endif
     FUNC_LEAVE();
 }
 
