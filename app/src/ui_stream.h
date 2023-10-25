7f53a83c020ea50dc693d96646c8bb881a2f786a
Sébastien Palmer <sebastien.palmer@functori.com>
2023-10-25 17:34:04 +0200

    Change the back settings screen icon to back arrow

--- app/src/ui_stream.h
+++ app/src/ui_stream.h
@@ -92,6 +92,7 @@ typedef uint8_t tz_ui_icon_t;
 #define TZ_UI_ICON_CROSS     0x02
 #define TZ_UI_ICON_DASHBOARD 0x03
 #define TZ_UI_ICON_SETTINGS  0x04
+#define TZ_UI_ICON_BACK      0x05
 
 typedef struct {
     tz_ui_icon_t    icon;
