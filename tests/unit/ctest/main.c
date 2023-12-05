9d0c11c12db8cc5411696b40fbeb4924012b2840
Emma Turner <emma.turner@trili.tech>
2023-12-05 16:29:28 +0000

    tests: boostrap ctest framework
    Taken from commit https://github.com/bvdberg/ctest commit 2cb74ff334da24d1ac783eefa8113bc4c8980ef9.

---
+++ tests/unit/ctest/main.c
@@ -0,0 +1,33 @@
+/* Copyright 2011-2023 Bas van den Berg
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <stdio.h>
+
+#define CTEST_MAIN
+
+// uncomment lines below to enable/disable features. See README.md for details
+#define CTEST_SEGFAULT
+//#define CTEST_NO_COLORS
+//#define CTEST_COLOR_OK
+
+#include "ctest.h"
+
+int main(int argc, const char *argv[])
+{
+    int result = ctest_main(argc, argv);
+
+    printf("\nNOTE: some tests will fail, just to show how ctest works! ;)\n");
+    return result;
+}
