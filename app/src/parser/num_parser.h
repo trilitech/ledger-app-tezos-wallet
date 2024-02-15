d2b3274813cb394047c2a407101f3d8ab2fc42dd
Sébastien Palmer <sebastien.palmer@functori.com>
2024-02-15 12:04:49 +0100

    Documentation: add documentation for number parser

--- app/src/parser/num_parser.h
+++ app/src/parser/num_parser.h
@@ -19,14 +19,55 @@
 
 #include "parser_state.h"
 
-void             tz_parse_num_state_init(tz_num_parser_buffer *buffers,
-                                         tz_num_parser_regs   *regs);
+/**
+ * @brief Initialize a num state buffer and a num state regs
+ *
+ * @param buffers: number parser buffers
+ * @param regs: number parser register
+ */
+void tz_parse_num_state_init(tz_num_parser_buffer *buffers,
+                             tz_num_parser_regs   *regs);
+
+/**
+ * @brief Parse one byte
+ *
+ * @param buffers: number parser buffers
+ * @param regs: number parser register
+ * @param b: byte to parse
+ * @param natural: if the number to read is natural
+ * @return tz_parser_result: parser result
+ */
 tz_parser_result tz_parse_num_step(tz_num_parser_buffer *buffers,
                                    tz_num_parser_regs *regs, uint8_t b,
                                    bool natural);
+
+/**
+ * @brief Parse one byte to read an integer
+ *
+ * @param buffers: number parser buffers
+ * @param regs: number parser register
+ * @param b: byte to parse
+ * @return tz_parser_result: parser result
+ */
 tz_parser_result tz_parse_int_step(tz_num_parser_buffer *buffers,
                                    tz_num_parser_regs *regs, uint8_t b);
+
+/**
+ * @brief Parse one byte to read a natural number
+ *
+ * @param buffers: number parser buffers
+ * @param regs: number parser register
+ * @param b: byte to parse
+ * @return tz_parser_result: parser result
+ */
 tz_parser_result tz_parse_nat_step(tz_num_parser_buffer *buffers,
                                    tz_num_parser_regs *regs, uint8_t b);
 
+/**
+ * @brief format a buffer to mutez number
+ *
+ * @param in: intput buffer
+ * @param out: output number
+ * @return bool: success
+ */
 bool tz_string_to_mutez(const char *in, uint64_t *out);
--- app/src/parser/num_state.h
+++ app/src/parser/num_state.h
@@ -18,14 +18,23 @@
 
 #include "formatting.h"
 
+/**
+ * @brief This struct represents the register for the parser of a number
+ *
+ *        The parser is a one-by-one byte reader.
+ */
 typedef struct {
-    uint16_t size;
-    uint8_t  sign : 1, stop : 1;
+    uint16_t size;      /// size of the number
+    uint8_t  sign : 1;  /// sign ot the number
+    uint8_t  stop : 1;  /// number as been fully parsed
 } tz_num_parser_regs;
 
-#define TZ_NUM_BUFFER_SIZE 256
+#define TZ_NUM_BUFFER_SIZE 256  /// Size of the number buffer
 
+/**
+ * @brief This struct represents the output buffers for the parser of a number
+ */
 typedef struct {
-    uint8_t bytes[TZ_NUM_BUFFER_SIZE / 8];
-    char    decimal[TZ_DECIMAL_BUFFER_SIZE(TZ_NUM_BUFFER_SIZE / 8)];
+    uint8_t bytes[TZ_NUM_BUFFER_SIZE / 8];                         /// bytes
+    char decimal[TZ_DECIMAL_BUFFER_SIZE(TZ_NUM_BUFFER_SIZE / 8)];  /// decimal
 } tz_num_parser_buffer;
