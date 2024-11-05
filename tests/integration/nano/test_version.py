8a85c13ce58159b512350060ae97a40041bde86c
Sébastien Palmer <sebastien.palmer@functori.com>
2024-11-05 11:01:17 +0100

    [test] merge git/version files

--- tests/integration/nano/test_version.py
+++ tests/integration/nano/test_version.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-# Copyright 2023 Functori <contact@functori.com>
+# Copyright 2024 Functori <contact@functori.com>
+# Copyright 2024 Trilitech <contact@trili.tech>
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Module to test version instruction."""
+"""Gathering of tests related to app version."""
 
 from utils.app import Screen, TezosAppScreen
 
@@ -24,5 +25,15 @@ def test_version(app: TezosAppScreen):
     data = app.backend.version()
 
     app.checker.check_version(data)
+
+    app.quit()
+
+def test_git(app: TezosAppScreen):
+    """Test that the app commit is the same as the current git commit."""
+    app.assert_screen(Screen.HOME)
+
+    data = app.backend.git()
+
+    app.checker.check_commit(data)
 
     app.quit()
